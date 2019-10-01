e33c844b30b5e60742ce4c506f3c04cec660db4d
evecmd <836069784@qq.com>
2019-10-01 08:58:53 +0800

    ke1

---
+++ src/MainClass.java
@@ -0,0 +1,14 @@
+import course.*;
+import student.*;
+import teacher.*;
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		Course cs = new Course();
+		Student st = new Student();
+		Teacher te = new Teacher();
+
+	}
+
+}
